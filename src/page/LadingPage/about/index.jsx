1f840784eeb4fb20d983d2d143f782265df4570a
Francisco Diakomas <franciscodiakoma@gmail.com>
2024-09-29 03:31:13 +0100

    Termieni o FrontEnd , estou a trabalhar agora no Backend

--- src/page/LadingPage/about/index.jsx
+++ src/page/LadingPage/about/index.jsx
@@ -2,11 +2,51 @@
 import './ndex.css'
 import chefe from '../../assets/chef5.png'
 import { FaPaperPlane } from 'react-icons/fa';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import { useEffect } from 'react';
 export default function AboutUs() {
+
+    useEffect(()=>{
+
+
+        gsap.registerPlugin(ScrollTrigger)
+        gsap.to('.textAbout', {
+            x : 0,
+            opacity : 1,
+            rotate : '0deg',
+            scrollTrigger : {
+                trigger : '#about',
+                scrub : true,
+                start : 'top 750px',
+                end : 'bottom 700px',
+            }
+        })
+        gsap.to('.imgAbout', {
+            x : 0,
+            opacity : 1,
+            rotate : '0deg',
+            scrollTrigger : {
+                trigger : '#about',
+                scrub : true,
+                start : 'top 750px',
+                end : 'bottom 700px',
+            }
+        })
+
+        return () => {
+
+            gsap.killTweensOf(".textAbout")
+            gsap.killTweensOf(".imgAbout")
+        }
+    })
+
+
+
     return (
     <>
         <section id='about'>
-            <article>
+            <article className='textAbout'>
                 <p>Sobre Nós</p>
                 <h1>
                 Bem-vindo ao <span>FastFood</span>, onde a paixão pela culinária encontra a conveniência do <span>delivery</span>.
@@ -15,7 +55,7 @@ export default function AboutUs() {
                 </span>
                 <button>Baixar Cv</button>
             </article>
-            <article>
+            <article className='imgAbout'>
                 <img src={chefe} loading='lazy' />
             </article>
         </section>
